c3fd583d4a5514600ed5af8a9b2388b73c9bc0d6
Danny Cairns <dpcairns@gmail.com>
2020-05-06 15:08:47 -0700

    add simple error handling

--- index.js
+++ index.js
@@ -13,22 +13,35 @@ app.use(cors());
 
 
 app.get('/location', async(req, res) => {
-    console.log('=============================\n');
-    console.log('|| PORT', PORT);
-    console.log('\n=============================');
-    const data = await request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${req.query.search}&format=json`);
+    try {
+        const data = await request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${req.query.search}&format=json`);
+    
+        const mungedData = mungeLocation(data.body);
+        res.json(mungedData);
+    } catch (e) {
+        res.json({
+            status: 500,
+            responseText: 'Sorry, something went wrong',
+            e,
+        });
+    }
 
-    const mungedData = mungeLocation(data.body);
-
-    res.json(mungedData);
 });
 
 app.get('/weather', async(req, res) => {
-    const data = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${req.query.latitude}&lon=${req.query.longitude}&key=${process.env.WEATHER_KEY}`);
-
-    const mungedData = mungeWeather(data.body);
-
-    res.json(mungedData);
+    try {
+        const data = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${req.query.latitude}&lon=${req.query.longitude}&key=${process.env.WEATHER_KEY}`);
+    
+        const mungedData = mungeWeather(data.body);
+    
+        res.json(mungedData);
+    } catch (e) {
+        res.json({
+            status: 500,
+            responseText: 'Sorry, something went wrong',
+            e,
+        });
+    }
 });
 
 
